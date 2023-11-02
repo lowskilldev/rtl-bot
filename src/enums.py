2e8f497797125ed8b075edd8b9c3eccd42ee39c2
linius <146680754+lowskilldev@users.noreply.github.com>
2023-11-02 16:34:12 +0300

    Create enums.py

---
+++ src/enums.py
@@ -0,0 +1,15 @@
+from enum import Enum, auto
+
+class AutoName(str, Enum):
+    def _generate_next_value_(self, *args):
+        return self.lower()
+    
+    def __repr__(self):
+        return f"enums.{self}"
+    
+class GroupType(AutoName):
+    HOUR = auto()
+    DAY = auto()
+    MONTH = auto()
+
+__all__ = ["GroupType"]
